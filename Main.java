780e814f6b269cec90550198c0019a115c941efc
othmanamr <amr.othman@patelhs.org>
2020-11-05 17:52:04 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    int[] seats = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+    String[] names = new String[15];
+
+    for (int i = 0; i < names.length; i++) {
+      System.out.println("whats your name");
+     names[i] =  scan.next();
+    }
+  
+    while (students < 0)
+{
+   System.out.print(studets + "15");
+  i++;
+   
+    }
+  }
+}
